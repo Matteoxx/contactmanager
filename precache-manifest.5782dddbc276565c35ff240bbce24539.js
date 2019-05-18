1d1068f1efe97a3da4c36d512935693bc134b253
matteoxx <mateuszniziolek96@gmail.com>
2019-05-18 15:21:18 +0200

    Updates

---
+++ precache-manifest.5782dddbc276565c35ff240bbce24539.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65da78a256d0fc046ed85caf448ae0cb",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/contactmanager/static/js/2.760bc175.chunk.js"
+  },
+  {
+    "revision": "4d12e84c1479653cb940",
+    "url": "/contactmanager/static/js/main.b98c7456.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
